4650d948681acab310f0ff89d1e258072b007ea1
Patrick Waite <pwaite@umass.edu>
2023-10-16 15:38:00 -0400

    Inital commit for barcode linking project code for use with FOLIO

---
+++ databaseConnecttemplate.py
@@ -0,0 +1,13 @@
+#blank connection string to be use with sqlAchemy this can be modified based on the requiremets for the database being connected to
+
+#postGreSQL connection string
+def get_connectionString():
+    user = '' 
+    password = ''
+    host = '' 
+    port = ''
+    database = '' 
+    return "postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}".format(
+            user, password, host, port, database)
+
+    
